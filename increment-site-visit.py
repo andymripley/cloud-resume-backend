73b73104197cfdb9f2f4a2c404f7e7c27cd48f19
Andy Ripley <andymripley@gmail.com>
2022-07-26 22:35:19 -0400

    first commit

---
+++ increment-site-visit.py
@@ -0,0 +1,14 @@
+from google.cloud import firestore
+
+def increment(request):
+    incr_by = request.args.get('incr_by', 1)
+    
+    db = firestore.Client()
+
+    site_visit_ref = db.collection(u'cloud-resume-site').document(u'doc-site-visits')
+    site_visit_ref.update({'visits': firestore.Increment(incr_by)})
+    site_visit_get = site_visit_ref.get()
+    site_visit_dict = site_visit_get.to_dict()
+
+    return site_visit_dict
+
